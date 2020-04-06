aecddc6ee9abffe1602fbe880b4bfa86a56a7ee4
Jbud <Jbudn90@yahoo.com>
2020-04-06 14:25:26 -0600

    Cleaned population data - ready to extract data

--- src/main.py
+++ src/main.py
@@ -3,9 +3,9 @@
 
 '''
 To Do:
-CLean Unnamed Column - Create Country Column, extract names
-
-
+Make separate DataFrame for energy use - only data for renewable vs non renewable
+Gitignore scripts - start
+Get population threshold to narrow down hypothesis test
 '''
 import numpy as np
 import pandas as pd
@@ -27,7 +27,12 @@ plt.style.use('ggplot')
 start_year = 2000
 end_year = 2017
 
-#Import electricity generation data and clean it
+population_cutoff = 20000000
+'''
+-----------------------------------------------------------------
+Import electricity generation data and clean it
+-----------------------------------------------------------------
+'''
 energy_data = pd.read_csv('data/INT-Export-04-05-2020_00-10-38.csv', header=1)
 energy_data.rename(columns={'Unnamed: 1': 'Energy Type', 'API': 'Country Code'}, inplace = True)
 countries = energy_data['Energy Type'][0::15]
@@ -40,18 +45,46 @@ for code in energy_data['Country Code']:
     else:
         country_code.append(code)
 energy_data['Country Code'] = country_code
+energy_data['Country Code'][energy_data['Country Code'] == 'WORL'] = 'WLD'
 
-#Countries provides another database to translate country codes into Country Names
+#Countries provides another dataframe to translate country codes into Country Names
 country_code = energy_data['Country Code'].copy()
 country_code.index -= 1
 countries = pd.DataFrame(countries).join(country_code)
 countries.rename(columns ={'Energy Type': 'Country'})
 
 
-#Import population data and clean it
-population_data = pd.read_csv('data/API_SP.POP.csv', header=4)
-population_data.drop(['Unnamed: 64', 'Indicator Name',
+'''
+-----------------------------------------------------------------
+Import population data and clean it
+-----------------------------------------------------------------
+'''
+
+pop_data = pd.read_csv('data/API_SP.POP.csv', header=4)
+
+pop_data.drop(['Unnamed: 64', 'Indicator Name',
                       'Indicator Code', '2018', '2019'], axis=1, inplace=True)
+
+#Removing aggregated regions to avoid double counting
+regions_to_remove = (
+    'Arab World', 'Caribbean small states', 'Central Europe and the Baltics', 'Early-demographic dividend', 'East Asia & Pacific', 'East Asia & Pacific (excluding high income)', 'East Asia & Pacific (IDA & IBRD countries)',
+    'Europe & Central Asia (IDA & IBRD countries)', 'Euro area', 'Europe & Central Asia', 'Europe & Central Asia (excluding high income)', 'European Union', 'Fragile and conflict affected situations', 'Heavily indebted poor countries (HIPC)', 'IBRD only', 'IDA & IBRD total', 'IDA total', 'IDA blend', 'IDA only', 'Latin America & Caribbean', 'Latin America & the Caribbean (IDA & IBRD countries)', 'IDA only', 'Late-demographic dividend', 'Latin America & Caribbean (excluding high income)',
+    'Least developed countries: UN classification', 'Middle East & North Africa', 'Middle East & North Africa (excluding high income)', 'Middle East & North Africa (IDA & IBRD countries)', 'North America', 'Not classified', 'OECD members', 'Other small states', 'Pacific island small states', 'Pre-demographic dividend', 'Post-demographic dividend', 'Small states', 'South Asia', 'South Asia (IDA & IBRD)','Sub-Saharan Africa (IDA & IBRD countries)', 'Sub-Saharan Africa', 'Sub-Saharan Africa (excluding high income)', 'High income', 'Low & middle income', 'Low income', 'Lower middle income', 'Middle income', 'Upper middle income')
+
+pop_data.drop(pop_data[pop_data['Country Name'].isin(regions_to_remove)].index, inplace=True)
+
+#Modifying index so that aggregated World row is placed on top, rest are alphabetical order, removing invalid entries
+
+pop_data.sort_values('Country Name', inplace=True)
+pop_data.fillna(0, inplace=True)
+pop_data.reset_index(drop = True, inplace = True)
+pop_data.index += 1
+pop_data.loc[0] = pop_data.loc[215]
+pop_data.drop(215, inplace = True)
+pop_data.sort_index(inplace=True)
+pop_data.reset_index(drop=True, inplace=True)
+
+#pop_data[pop_data['Country Name'] == 'United States']
 
 #Percentage of renewable electricity of whole world
 percentage = energy_data.iloc[4][2:].astype(
