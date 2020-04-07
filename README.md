e8826408f85149bcf8ba27f1f8eb5f7768be95fd
Jbud <Jbudn90@yahoo.com>
2020-04-07 16:13:04 -0600

    Running Code - Established developed countries as main focus. Attempting to separate into classes, change years of data from strings into integers.

--- README.md
+++ README.md
@@ -8,3 +8,11 @@ World Energy Use:
 
 World Population:
 https://data.worldbank.org/indicator/sp.pop.totl
+
+https://worldpopulationreview.com/countries/developed-countries/
+
+https://www.un.org/en/development/desa/policy/wesp/wesp_current/2014wesp_country_classification.pdf
+
+To Tell Frank:
+
+Changing project slightly - rather than choose a population threshold for developed countries (India is less developed but very high population - getting a dataset from UN that classifies developed vs developing and using those developed countries for hypothesis - will still look at population data though)
--- src/main.py
+++ src/main.py
@@ -2,8 +2,23 @@
 #Functions : Plot graphs
 
 '''
+To get year columns mapped as int:
+    dtype in read csv, import dictionary with column names
+
+Bring in list of developed vs developing countries
+color code 
+
 To Do ++ :
 Left Justify Energy Type column
+Tuesday:
+Finish Hypo Test
+plots
+
+Wednesday
+MVP+ Stuff
+
+Thursday:
+Clean Code, Work on readme
 
 To Do:
 Make separate DataFrame for energy use - only data for renewable vs non renewable, add to highest population table
@@ -12,6 +27,18 @@ hypo test
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+
+""" 
+class analysis(object):
+    def __init__(self, title, data_x, data_ylist):
+        self.title = title
+        self.data_x = data_x
+        self.data_ylist = data_ylist
+
+    def plot_data() 
+    
+"""
+
 
 #The original energy data is formatted with leading spaces to identify subgroups of each type. This function iterates through and replaces these using numerical indexing for readbility in DataFrames.
 
@@ -30,6 +57,8 @@ def indent_replace(a_series):
         elif space_count >= 12:
             type2 += 1
             new_series.append('{}'.format(type1) + '.' + '{}'.format(type2) + '.' + no_indent)
+        else:
+            print('error, ommitted {}'.format(string))
     return new_series
 
 def col_to_num(df, col1, col2 = None):
@@ -46,23 +75,26 @@ def col_to_num(df, col1, col2 = None):
 #     return df.to_string(formatters = formatters, index = False)
 
 def make_plots(data, subpltrows = 1, subpltcols = 1):
-    fig, axes = plt.subplots(subpltrows, subpltcols, figsize=(12, 6))
+    fig, axes = plt.subplots(subpltrows, subpltcols, figsize=(14, 6))
     x = data.index
     y = [float(data[val]) for val in x]
     axes.scatter(x, y)
-    fig.tight_layout(pad=1)
+    fig.tight_layout(pad = 3)
     axes.grid(True)
     return fig, axes
 
 plt.style.use('ggplot')
 
 #Set parameters
-start_year = '2000'
+start_year = '1980'
 end_year = '2017'
+years_to_int = {str(year): int(year) for year in range(
+    int(start_year), int(end_year) + 1)}
 
-#Assumes that countries with highest populations are developed countries - this number sets the number of countries to analyze, and will calculate a population threshold of developing/developed.
-# Set to 155 or less
+#This number sets the number of countries to analyze per top in each category.
 countries_to_analyze = 20 + 1
+
+
 
 '''
 -----------------------------------------------------------------
@@ -71,23 +103,31 @@ Import electricity generation data and clean it
 '''
 energy_data = pd.read_csv('data/INT-Export-04-05-2020_00-10-38.csv', header=1)
 energy_data.rename(columns={'Unnamed: 1': 'Energy Type', 'API': 'Country Code'}, inplace = True)
-countries = energy_data['Energy Type'][0::15]
 # 2018 has a lot of missing data
 energy_data.drop('2018', axis=1, inplace=True)
 
-#Original dataset has several rows that are totals of other forms of energy. This section drops the rows of totals so that only renewable energy has subgroups.
+#Original dataset has several rows that are totals of other forms of energy. This section drops the rows of totals so that only renewable energy has subgroups. Several countries are missing a country code and these are specified manually.
+energy_data['Country Code'].loc[1981:1994] = 'Micronesia'
+energy_data['Country Code'].loc[2297:2309] = 'MNP'
+energy_data['Country Code'].loc[3181:3194] = 'Tuvalu'
+energy_data['Country Code'].loc[3211:3224] = 'U.S. Territories'
+countries = energy_data['Energy Type'][0::15]
 energy_data.drop(energy_data.loc[0::15].index, inplace=True)
 energy_data.drop(energy_data.loc[6::14].index, inplace=True)
 energy_data.drop(energy_data.loc[8::13].index, inplace=True)
 energy_data.drop(energy_data.loc[9::12].index, inplace=True)
 energy_data.reset_index(drop = True, inplace = True)
 
+energy_data.drop(
+    energy_data[energy_data['Country Code'] == 'none'].index, inplace=True)
+
 country_code = []
 for code in energy_data['Country Code']:
-    if type(code) == str and code.count('-') >= 3:
+    if code != 'none' and type(code) == str and code.count('-') >= 3:
         country_code.append(code.split('-')[2])
     else:
         country_code.append(code)
+
 energy_data['Country Code'] = country_code
 energy_data['Country Code'][energy_data['Country Code'] == 'WORL'] = 'WLD'
 energy_data['Energy Type'] = indent_replace(energy_data['Energy Type'])
@@ -96,12 +136,28 @@ energy_data.replace('--', 0, inplace = True)
 energy_data.iloc[:, 2:] = energy_data.iloc[:, 2:].astype('float')
 
 #Countries provides another dataframe to translate country codes into Country Names
+countries.reset_index(drop=True, inplace=True)
 country_code = energy_data['Country Code'].copy()
-country_code.index -= 1
-countries = pd.DataFrame(countries).join(country_code)
-countries.rename(columns ={'Energy Type': 'Country'})
+country_code.drop_duplicates(inplace=True)
+country_code.reset_index(drop = True, inplace = True)
+countries = pd.DataFrame(country_code).join(countries)
+countries.rename(columns ={'Energy Type': 'Country'}, inplace = True)
+energy_data.merge(countries)
 
-
+#Replace Country codes with country names
+energy_data = energy_data.iloc[:, :1].merge(countries).join(energy_data.iloc[:, 1:])
+energy_data.drop('Country Code', axis=1, inplace = True)
+'''
+-----------------------------------------------------------------
+Import UN HDI data
+-----------------------------------------------------------------
+'''
+HDI_data = pd.read_csv('data/HDI.csv', nrows=190)
+HDI_data.drop(0, inplace = True)
+HDI_data['Country'] = HDI_data['Country'].str.lstrip()
+HDI_data.drop([HDI_data.columns[x] for x in range(
+    3, len(HDI_data.columns), 2)], axis=1, inplace=True)
+developed = HDI_data.sort_values('2017', ascending=False)['Country'][:20].reset_index(drop = True)
 '''
 -----------------------------------------------------------------
 Import population data and clean it
@@ -115,8 +171,7 @@ pop_data.drop(['Unnamed: 64', 'Indicator Name',
 
 #Removing aggregated regions to avoid double counting
 regions_to_remove = (
-    'Arab World', 'Caribbean small states', 'Central Europe and the Baltics', 'Early-demographic dividend', 'East Asia & Pacific', 'East Asia & Pacific (excluding high income)', 'East Asia & Pacific (IDA & IBRD countries)',
-    'Europe & Central Asia (IDA & IBRD countries)', 'Euro area', 'Europe & Central Asia', 'Europe & Central Asia (excluding high income)', 'European Union', 'Fragile and conflict affected situations', 'Heavily indebted poor countries (HIPC)', 'IBRD only', 'IDA & IBRD total', 'IDA total', 'IDA blend', 'IDA only', 'Latin America & Caribbean', 'Latin America & the Caribbean (IDA & IBRD countries)', 'IDA only', 'Late-demographic dividend', 'Latin America & Caribbean (excluding high income)',
+    'Arab World', 'Caribbean small states', 'Central Europe and the Baltics', 'Early-demographic dividend', 'East Asia & Pacific', 'East Asia & Pacific (excluding high income)', 'East Asia & Pacific (IDA & IBRD countries)','Europe & Central Asia (IDA & IBRD countries)', 'Euro area', 'Europe & Central Asia', 'Europe & Central Asia (excluding high income)', 'European Union', 'Fragile and conflict affected situations', 'Heavily indebted poor countries (HIPC)', 'IBRD only', 'IDA & IBRD total', 'IDA total', 'IDA blend', 'IDA only', 'Latin America & Caribbean', 'Latin America & the Caribbean (IDA & IBRD countries)', 'IDA only', 'Late-demographic dividend', 'Latin America & Caribbean (excluding high income)',
     'Least developed countries: UN classification', 'Middle East & North Africa', 'Middle East & North Africa (excluding high income)', 'Middle East & North Africa (IDA & IBRD countries)', 'North America', 'Not classified', 'OECD members', 'Other small states', 'Pacific island small states', 'Pre-demographic dividend', 'Post-demographic dividend', 'Small states', 'South Asia', 'South Asia (IDA & IBRD)','Sub-Saharan Africa (IDA & IBRD countries)', 'Sub-Saharan Africa', 'Sub-Saharan Africa (excluding high income)', 'High income', 'Low & middle income', 'Low income', 'Lower middle income', 'Middle income', 'Upper middle income')
 
 pop_data.drop(pop_data[pop_data['Country Name'].isin(regions_to_remove)].index, inplace=True)
@@ -138,7 +193,7 @@ pop_data.reset_index(drop=True, inplace=True)
 Highest population dataset determined here
 --------------------------------------------
 '''
-developed = pop_data.sort_values('1980',ascending = False).copy()[1: countries_to_analyze]
+high_pop = pop_data.sort_values('1980',ascending = False).copy()[1: countries_to_analyze]
 
 #Percentage of renewable electricity of whole world
 percentage = energy_data.iloc[4][2:].astype(
@@ -146,12 +201,27 @@ percentage = energy_data.iloc[4][2:].astype(
 plt.scatter(percentage.index, percentage)
 
 #Worldwide use of renewable energy
-world_fig, world_axes = make_plots(energy_data.iloc[4].iloc[2:])
-#world_axes.set_ylim(0, energy_data['2017'].max())
-world_fig.show()
+show_plot = 'F'
+if show_plot == 'T':
+    world_fig, world_axes = make_plots(energy_data.iloc[4].iloc[2:])
+    world_axes.set_xlabel('Year')
+    world_axes.xaxis.set_ticks(np.arange(0,int(end_year)-int(start_year), 2))
+    world_axes.set_xlim(-1, 38)
+    world_axes.set_ylabel('Billion Kwh produced')
+    world_axes.set_title('Worldwide Renewable Electricity Production')
+    world_fig.show()
 
 #World Population
-#make_plots(population_data.sum().iloc[2:])
+make_plots(pop_data.sum().iloc[2:])
 
+
+# Plot of top 20 most developed countries renewable energies
+developed = pd.DataFrame(developed).merge(energy_data)
+fig, ax = plt.subplots(1,1, figsize = (12,6))
+
+# Norway_reuse = developed_energy.
+# ax.plot(developed_energy.columns, )
+
+#lt.plot(developed_energy.iloc[3, 1:].index[1:], developed_energy.iloc[3, 1:].values[1:])
 #if __name__ == '__main__':
 #    print(energy_data)
